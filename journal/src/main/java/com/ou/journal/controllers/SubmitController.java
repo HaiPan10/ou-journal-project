e21697462efb32297fa09876e0c887e60368a221
NganBao0206 <yuu.mmd02@gmail.com>
2023-10-24 00:03:04 +0700

    [Ngan] - step2submit

--- journal/src/main/java/com/ou/journal/controllers/SubmitController.java
+++ journal/src/main/java/com/ou/journal/controllers/SubmitController.java
@@ -100,6 +100,9 @@ public class SubmitController {
 
     @GetMapping({ "/submit/step2" })
     public String getSubmitPage2(@ModelAttribute("article") Article article, Model model) {
+        if (article.getAbstracts() == null || article.getAbstracts().isBlank() || article.getTitle() == null || article.getTitle().isBlank()) {
+            return "redirect:/submit/step1";
+        }
         List<Object[]> users = userService.listUser();
         model.addAttribute("users", users);
         return "client/submitManuscript/step2";
@@ -107,7 +110,23 @@ public class SubmitController {
 
     @GetMapping({ "/submit/step3" })
     public String getSubmitPage3(@ModelAttribute("article") Article article, Model model) {
-        if (article.getFile() != null) {
+        if (article.getAbstracts() == null || article.getAbstracts().isBlank() || article.getTitle() == null || article.getTitle().isBlank()) {
+            return "redirect:/submit/step1";
+        } else {
+            final boolean[] isHasFirstAuthor = {false};
+            final boolean[] isHasCorresponding = {false};
+            article.getAuthorArticles().forEach(authorArticle -> {
+                authorArticle.getAuthorRoles().forEach(role -> {
+                    if (role.getAuthorType().getId() == 1) 
+                        isHasFirstAuthor[0] = true;
+                    else if (role.getAuthorType().getId() == 2)
+                        isHasCorresponding[0] = true;
+                });
+            });
+            if (!isHasFirstAuthor[0] || !isHasCorresponding[0])
+                return "redirect:/submit/step2";
+        }
+        if (article.getFile() != null && !article.getFile().isEmpty()) {
             String originalFilename = article.getFile().getOriginalFilename();
             String extension = FilenameUtils.getExtension(originalFilename);
             model.addAttribute("extension", extension);
