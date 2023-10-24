f2769a29af743999b670ed47918470d78c39aa8b
NganBao0206 <yuu.mmd02@gmail.com>
2023-10-24 18:54:44 +0700

    [Ngan] - register

--- journal/src/main/java/com/ou/journal/controllers/RegisterController.java
+++ journal/src/main/java/com/ou/journal/controllers/RegisterController.java
@@ -1,15 +1,29 @@
 package com.ou.journal.controllers;
 
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.ou.journal.configs.JwtService;
+import com.ou.journal.enums.AccountStatus;
+import com.ou.journal.enums.SecrectType;
+import com.ou.journal.pojo.Account;
+import com.ou.journal.pojo.User;
 import com.ou.journal.service.interfaces.AccountService;
 import com.ou.journal.service.interfaces.UserService;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class RegisterController {
@@ -19,36 +33,57 @@ public class RegisterController {
     @Autowired
     AccountService accountService;
 
-     @GetMapping("/register")
+    @Autowired
+    JwtService jwtService;
+
+    @GetMapping("/register")
     public String registerPage(
-        @RequestParam(required = false, name = "token") String token) {
-        if (token == null)
-            return "client/registerEmail";
-        else {
-            //Kiểm tra token 
-            //Hợp lệ return
-             return "client/register";
-             //Khong hop lẹe thì return "client/registerEmail";
+            @RequestParam(required = false, name = "token") String token, Model model) {
+        if (token != null) {
+            // Kiểm tra token
+            String email = jwtService.getEmailFromToken(token, SecrectType.EMAIL);
+            if (email != null) {
+                User user = new User();
+                Account account = new Account();
+                user.setAccount(account);
+                user.setEmail(email);
+                model.addAttribute("user", user);
+                model.addAttribute("token", token);
+                return "client/register";
+            }
         }
+        return "client/registerEmail";
     }
 
+    // Bước kiểm tra email
+    @PostMapping("/register/email")
+    public String submitEmail(@RequestPart("email") String email, RedirectAttributes redirectAttributes) {
+        try {
+            if (accountService.getByEmail(email).isPresent()) {
+                redirectAttributes.addFlashAttribute("errorMessage", "Email đã được sử dụng, vui lòng nhập email khác");
+                return "redirect:/register";
+            } else if (userService.findByEmail(email) != null) {
+                //Xử lý gửi mail tạo tài khoản
+                return "hehe";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        String token = jwtService.generateEmailToken(null, null, email);
+        System.out.println(token);
+        return String.format("redirect:/register?token=%s", token);
+    }
+
+    // Người dùng chưa có trong hệ thống gửi thông tin đăng ký
     @PostMapping("/register")
-    public String submitRegister(@RequestBody String email, RedirectAttributes redirectAttributes) throws Exception {
-        //Kiểm tra email
-        if(accountService.getByEmail(email) != null) {
-            redirectAttributes.addFlashAttribute("errorMessage", "Email đã được sử dụng, vui lòng nhập email khác");
-            return "redirect:/register";
-        } else if (userService.findByEmail(email) != null) {
-            //Xử lý gửi mail
-            return "hehe";
+    public String submitRegister(@ModelAttribute("user") @Valid User u, BindingResult rs) {
+        if (!rs.hasErrors()) {
+            u.getAccount().setEmail(u.getEmail());
+            u.getAccount().setStatus(AccountStatus.ACCEPTED.toString());
+           //u.setUserRoles()
         }
-        
-        //Tạo token
-        //
-        //
-        //Tạo token giả bộ
-        String token = "tokenne";
-
-         return String.format("redirect:/client/registerEmail?token=%s", token);
+        return "client/register";
     }
+
 }
