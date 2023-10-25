f746b881b79db5bca1557e31890bbd7f3748ecf3
NganBao0206 <yuu.mmd02@gmail.com>
2023-10-26 00:17:38 +0700

    [Ngan] - style header

--- journal/src/main/resources/static/js/header.js
+++ journal/src/main/resources/static/js/header.js
@@ -11,28 +11,31 @@ function handleChange(event) {
   }
 }
 
-function callChangeRoleApi() {
-  let roleName = document.getElementById("roleName").value
-  console.log(roleName)
-  fetch("/api/accounts/change-role", {
-    headers: {
-      "Content-Type": "application/json"
-    },
-    method: "POST",
-    body: JSON.stringify({
-      roleName: roleName
+function callChangeRoleApi(obj) {
+  if (confirm("Bạn có chắc chắn muốn đổi vai trò không")) {
+    let roleName = obj.getAttribute("data-role");
+    console.log(roleName)
+    fetch("/api/accounts/change-role", {
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "POST",
+      body: JSON.stringify({
+        roleName: roleName
+      })
+    }).then(response => {
+      if(response.status === 204){
+        window.location.reload()
+      } else if(response.status === 403){
+        alert("Không có vai trò đó")
+      } else {
+        throw new Error()
+      }
+    }).catch(err => {
+      alert("Something Wrong")
     })
-  }).then(response => {
-    if(response.status === 204){
-      window.location.reload()
-    } else if(response.status === 403){
-      alert("Không có vai trò đó")
-    } else {
-      throw new Error()
-    }
-  }).catch(err => {
-    alert("Something Wrong")
-  })
+  }
+ 
 }
 
 // Gắn hàm xử lý vào sự kiện 'change'
