e6cb28ed1b63a1ea469d9389a6ed1974ac018a04
HaiPan10 <phanthanhhai3317@gmail.com>
2023-11-09 17:50:37 +0700

    [Hai] - api reinvite reviewer

--- journal/src/main/java/com/ou/journal/api/ApiReviewArticleController.java
+++ journal/src/main/java/com/ou/journal/api/ApiReviewArticleController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +24,10 @@ import org.springframework.web.multipart.MultipartFile;
 import com.ou.journal.configs.JwtService;
 import com.ou.journal.enums.ReviewArticleStatus;
 import com.ou.journal.enums.SecrectType;
+import com.ou.journal.pojo.Article;
 import com.ou.journal.pojo.AuthenticationUser;
+import com.ou.journal.pojo.User;
+import com.ou.journal.service.interfaces.ArticleService;
 import com.ou.journal.service.interfaces.ReviewArticleService;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -39,6 +43,9 @@ public class ApiReviewArticleController {
 
     @Autowired
     private Environment environment;
+
+    @Autowired
+    private ArticleService articleService;
 
     @GetMapping("/response")
     public ResponseEntity<?> responseReview(@RequestParam String status, @RequestParam String token, HttpServletRequest httpServletRequest) throws Exception{
@@ -86,6 +93,16 @@ public class ApiReviewArticleController {
         MultipartFile reviewFile, String status, @AuthenticationPrincipal AuthenticationUser currentUser) throws Exception{
         try {
             return ResponseEntity.ok().body(reviewArticleService.doneReview(reviewArticleId, currentUser.getId(), reviewFile, status));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/reviewer/reinvite/{articleId}")
+    public ResponseEntity<?> reinviteReviewer(@PathVariable Long articleId, @RequestBody User user){
+        try {
+            Article article = articleService.retrieve(articleId);
+            return ResponseEntity.ok().body(reviewArticleService.create(user, article));
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
