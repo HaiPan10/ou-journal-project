ad3ee52a0350eb2e4c36932f1ab793a1c33bc2e9
PlaIsMe <phongvulai96@gmail.com>
2023-11-14 17:35:05 +0700

    [Phong] - commit for pull

--- journal/src/main/java/com/ou/journal/controllers/ReviewerController.java
+++ journal/src/main/java/com/ou/journal/controllers/ReviewerController.java
@@ -8,8 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import com.ou.journal.enums.ReviewArticleStatus;
 import com.ou.journal.pojo.AuthenticationUser;
 import com.ou.journal.pojo.ReviewArticle;
